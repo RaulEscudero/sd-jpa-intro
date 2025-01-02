a33ce3382a3eab6b9c2c074cbb880b459456f008
Raul Escudero <raul.escudero@ciberexperis.es>
2025-01-02 18:02:19 +0100

    init data with initializer

--- src/test/java/guru/springframework/sdjpaintro/SpringBootJpatestSlice.java
+++ src/test/java/guru/springframework/sdjpaintro/SpringBootJpatestSlice.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.Rollback;
 
@@ -15,6 +16,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
+@ComponentScan(basePackages = {"guru.springframework.sdjpaintro.bootstrap"})
 public class SpringBootJpatestSlice {
 
     @Autowired
@@ -26,7 +28,7 @@ public class SpringBootJpatestSlice {
     @Test
     void testJpaTestSplice(){
         long countBefore = bookRepository.count();
-        assertThat(countBefore).isEqualTo(0);
+        assertThat(countBefore).isEqualTo(2);
 
         bookRepository.save(new Book("My book", "456", "Self)"));
         long countAfter = bookRepository.count();
@@ -38,7 +40,7 @@ public class SpringBootJpatestSlice {
     @Test
     void testJpaTestSpliceTransaction(){
         long countBefore = bookRepository.count();
-        assertThat(countBefore).isEqualTo(1);
+        assertThat(countBefore).isEqualTo(3);
 
     }
 
